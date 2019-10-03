0cd4f1785a159e3bb0fbc36021af720b06f9ade7
Kostya_dev <serviceafcstudio@yandex.ru>
2019-10-03 16:20:23 +0500

    Created menu list

---
+++ assets/js/mainaction.js
@@ -0,0 +1,26 @@
+// var ObjectInterface =
+//     {
+//       MenuButton: document.getElementById('menu_buttom')
+//     };
+// var UserExperience =
+//     {
+//         MenuShow: function (button) {
+//             alert(1);
+//             button.visibility = "hidden";
+//         }
+//     };
+
+function showMenu() {
+   $('#menu').toggleClass('menu-active');
+   if ($('#menu').hasClass('menu-active')) {
+       $('#menu_buttom :first-child').css("transform", "rotate(-45deg)");
+       $('#menu_buttom :last-child').css("transform", "rotate(45deg)");
+       $('#menu_buttom :nth-child(2)').css("display", "none");
+   }
+   else
+   {
+       $('#menu_buttom :first-child').css("transform", "rotate(0deg)");
+       $('#menu_buttom :last-child').css("transform", "rotate(0deg)");
+       $('#menu_buttom :nth-child(2)').css("display", "block");
+   }
+}
